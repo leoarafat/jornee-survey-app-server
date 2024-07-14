033d33d666f9caf4f2ec359015a81aff5e27a41b
Yeasin Arafat <leoarafat10@gmail.com>
2024-07-14 11:13:46 +0600

    create test and test item

--- src/app/modules/test/test.service.ts
+++ src/app/modules/test/test.service.ts
@@ -1,10 +1,24 @@
-import { ITest } from './test.interface';
+import ApiError from '../../../errors/ApiError';
+import { ITest, ITestItem } from './test.interface';
+import { Test, TestItem } from './test.model';
 
 const createTest = async (payload: ITest) => {
-  console.log(payload);
+  const isExistTest = await Test.findOne({ name: payload.name });
+  if (isExistTest) {
+    throw new ApiError(400, 'Test already exist');
+  }
+  return await Test.create(payload);
 };
-const createTestItem = async (payload: ITest) => {
-  console.log(payload);
+const createTestItem = async (payload: ITestItem) => {
+  const isExistTest = await Test.findOne({ name: payload.test });
+  if (!isExistTest) {
+    throw new ApiError(404, 'Test not found');
+  }
+  const isExistTestItem = await TestItem.findOne({ item: payload.item });
+  if (isExistTestItem) {
+    throw new ApiError(400, `${payload.item} already exist`);
+  }
+  return await TestItem.create(payload);
 };
 
 export const TestService = {
